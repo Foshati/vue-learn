f295bb45bb1267e4e14afb91771eeb17ede34e78
Foshati <amirrezafoshati@gmail.com>
2024-08-23 16:50:04 +0330

    learn v-if ,v-for ,v-bind...

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
 export default {
   data() {
     return {
-      name: 'sam foshati'
+      name: 'sam foshati',
+      status: 'active',
+      tasks: ['task one', 'task two', 'task three'],
+      link: 'https://google.com'
     }
   }
 }
@@ -10,10 +13,16 @@ export default {
 
 <template>
   <p>{{ name }}</p>
-</template>
 
-<style scoped>
-p {
-  color: red;
-}
-</style>
+  <p v-if="status === 'active'">user is active</p>
+  <p v-else-if="status === 'pending'">user is pending</p>
+  <p v-else>user is inactive</p>
+
+  <h3>tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <a v-bind:href="link">click for Google</a>
+  <a :href="link">click for Google2</a>
+</template>
