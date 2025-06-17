ed4f8df48f26c0d4cccafe6ac558088036bd5387
foshati <amirrezafoshati@gmail.com>
2025-06-17 23:26:58 +0330

    update for start project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
-<script>
-export default {
-  data() {
-    return {
-      name: 'sam foshati',
-      status: 'active',
-      tasks: ['task one', 'task two', 'task three'],
-      link: 'https://google.com'
-    }
-  }
-}
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <p>{{ name }}</p>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-  <p v-if="status === 'active'">user is active</p>
-  <p v-else-if="status === 'pending'">user is pending</p>
-  <p v-else>user is inactive</p>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
 
-  <h3>tasks:</h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
 
-  <a v-bind:href="link">click for Google</a>
-  <a :href="link">click for Google2</a>
+  <RouterView />
 </template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
